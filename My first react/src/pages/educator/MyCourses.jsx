30f218b9728dad1a500e00a83c44044657f583d2
YoussefElAraby <arabymohammedyoussef123@gmail.com>
2025-12-30 08:45:11 +0200

    Fetch dashboard data conditionally based on isEducator state in Dashboard, MyCourses, and StudentsEnrolled components

--- My first react/src/pages/educator/MyCourses.jsx
+++ My first react/src/pages/educator/MyCourses.jsx
@@ -23,10 +23,12 @@ const MyCourses = () => {
   }
 
   useEffect(() => {
-    fetchEducatorCourses()
-  }, []);
+    if(isEducator) {
+      fetchEducatorCourses()
+    }
+  }, [isEducator]);
 
-  return courses !== null ? (
+  return courses ? (
     <div className='h-screen flex flex-col items-start justify-between md:p-8 md:pb-0 p-4 pt-8 pb-0'>
       <div className='w-full'>
         <h2 className='pb-4 text-lg font-medium'>My Courses</h2>
--- My first react/src/pages/educator/StudentsEnrolled.jsx
+++ My first react/src/pages/educator/StudentsEnrolled.jsx
@@ -25,9 +25,11 @@ const StudentEnrolled = () => {
     }
   }
   useEffect(() => {
-    fetchEnrolledStudents()
-  }, []);
-  return enrolledStudents !== null ? (
+    if (isEducator) {
+      fetchEnrolledStudents()
+    }
+  }, [isEducator]);
+  return enrolledStudents ? (
     <div className='min-h-screen flex flex-col items-start justify-between md:p-8 md:pb-0 p-4 pt-8 pb-0'>
         <div className='flex flex-col items-center max-w-4xl w-full overflow-hidden rounded-md bg-white border border-gray-500/20'>
           <table className='table-fixed md:table-auto w-full overflow-hidden pb-4'>
